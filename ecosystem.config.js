edc1af63f77d562f3bacdd2edf5129c9db9f37fe
zzzamaris <283704057@qq.com>
2023-11-18 22:45:18 +0800

    部署

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+export const apps = [
+  {
+    name: 'zz-account-h5-server',
+    script: 'zz-account-h5-server.js'
+  },
+];
+export const deploy = {
+  production: {
+    user: 'zqy',
+    password: 'zqy123456',
+    host: '172.18.7.174',
+    ref: 'origin/master',
+    repo: 'git@github.com:Amaris10/zz-account-book.git',
+    path: '/workspace/zz-account-server-h5',
+    'post-deploy': 'git reset --hard && git checkout master && git pull && npm i --production=false && npm run build:release && pm2 startOrReload ecosystem.config.js',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }
+};
---
+++ zz-account-h5-server.js
@@ -0,0 +1,6 @@
+import { start } from 'pushstate-server'
+
+start({
+  port: 5021,
+  directory: './dist'
+})
